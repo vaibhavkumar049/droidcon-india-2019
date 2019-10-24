2f2b70a5db268ada6f961c8fab1a80406a87b876
Sayantan <sayanbis83@gmail.com>
2019-10-24 16:06:34 +0530

    Removes old suggestion chips and adds the ones with new featrues

--- droidcon-2019/functions/welcome.js
+++ droidcon-2019/functions/welcome.js
@@ -4,12 +4,12 @@ module.exports={
     welcome: function(conv){
         if(conv.user.last.seen){
             conv.ask(`Welcome Back ! How can I help you ?`);
-            conv.ask(new Suggestions(['Call For Speakers'],['Notify me'],['Register Now'],['View Details'],['About Venue']));
+            conv.ask(new Suggestions(['Speakers'],['Notify me'],['Register Now'],['View Details'],['About Venue']));
         }
         else{
             conv.ask(" Welcome to droid con India 2019");
             conv.ask("Consider me your guide to droid con India, I can help you to plan for droid con like when it is Happening or How to register for it.");
-            conv.ask(new Suggestions(['Call For Speakers'],['Notify me'],['View Details'],['Register Now'],['About Venue']));
+            conv.ask(new Suggestions(['Speakers'],['Notify me'],['View Details'],['Register Now'],['About Venue']));
 
         }
     }
